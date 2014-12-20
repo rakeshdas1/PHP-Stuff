a0193431489cf2cd9e589789ee5b2bdb73375e21
Rakesh Das <rakeshdaskowshik@gmail.com>
2014-12-19 18:58:58 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>title</title>
+  <link rel="stylesheet" href="stylesheet.css" type="text/css">
+ </head>
+ <body>
+     <p>First name:</p>
+     <br>
+     <input type="text" name="firstname">
+     <br>
+     <p>Last Name:</p>
+     <br>
+     <input type="text" name="lastname">
+     <br>
+     <p>Other names:</p>
+     <br>
+     <input type="text" name="othernames">
+  
+ </body>
+</html>
